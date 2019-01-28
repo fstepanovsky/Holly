d32a70edcc7824d8cd0008b43cb6b4e1020edc16
František Štěpanovský <fanda57@gmail.com>
2019-01-28 14:45:02 +0100

    simplified ctors

--- src/main/java/cz/mzk/holly/extractor/ImageExtractor.java
+++ src/main/java/cz/mzk/holly/extractor/ImageExtractor.java
@@ -3,37 +3,23 @@ package cz.mzk.holly.extractor;
 import cz.mzk.holly.fedora.FedoraRESTConnector;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Paths;
 
 public class ImageExtractor {
     // ToDo: Solve boilerplate code.
+
+    private static final String mzkBasePath = System.getenv("BASE_PATH_MZK");
+    private static final String ndkBasePath = System.getenv("BASE_PATH_NDK");
+
     public ImageExtractor(String uuid) {
 
-        if (System.getenv("BASE_PATH_MZK").isEmpty()) {
-            final String mzkBase = "/mzk";
-        } else {
-            final String mzkBase = System.getenv("BASE_PATH_MZK");
-        }
-        if (System.getenv("BASE_PATH_NDK").isEmpty()) {
-            final String ndkBase = "/ndk";
-        } else {
-            final String ndkBase = System.getenv("BASE_PATH_NDK");
-        }
         // ToDo: validate uuid
         // ToDo: mark as valid if valid
     }
 
     public ImageExtractor(String[] uuid) {
-
-        if (System.getenv("BASE_PATH_MZK").isEmpty()) {
-            final String mzkBase = "/mzk";
-        } else {
-            final String mzkBase = System.getenv("BASE_PATH_MZK");
-        }
-        if (System.getenv("BASE_PATH_NDK").isEmpty()) {
-            final String ndkBase = "/ndk";
-        } else {
-            final String ndkBase = System.getenv("BASE_PATH_NDK");
-        }
         // ToDo: validate uuids
         // ToDo: mark as valid if valid
     }
@@ -42,7 +28,10 @@ public class ImageExtractor {
         FedoraRESTConnector fedora = new FedoraRESTConnector();
         String imageUrl;
         try {
-            imageUrl = fedora.getImgAddressFromRels(uuid);
+            if (hasUuidPrefix(uuid))
+                imageUrl = fedora.getImgAddressFromRels(uuid);
+            else
+                return "";
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -53,8 +42,45 @@ public class ImageExtractor {
     }
 
     private String getPhysicalPath(String imgUrl) {
-        // ToDo: Parsing a translating path
-        return "";
+        String path;
+        try {
+            path =  new URL(imgUrl).getPath();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return "";
+        }
+
+        String physicalPath = "";
+
+        if (path.contains("/NDK"))
+        {
+            var pathComponents = path.split("/");
+            switch (pathComponents[2]) {
+                case "2012": case "2013":
+                    physicalPath = path.replace("NDK", "ndk01");
+                    break;
+                case "2014":
+                    physicalPath = path.replace("NDK", "ndk02");
+                    break;
+                case "2015": case "2016":
+                    physicalPath = path.replace("NDK", "ndk03");
+                    break;
+                case "2017": case "2018":
+                    physicalPath = path.replace("NDK", "ndk04");
+                    break;
+                case "2019":
+                    physicalPath = path.replace("NDK", "ndk2019");
+                    break;
+            }
+            physicalPath = Paths.get(ndkBasePath, physicalPath).toString();
+        } else {
+            physicalPath = Paths.get(mzkBasePath, path).toString();
+        }
+
+        if (path.contains(".tif"))
+            return physicalPath;
+        else
+            return physicalPath + ".jp2";
     }
 
     private boolean hasUuidPrefix(String uuid) {
