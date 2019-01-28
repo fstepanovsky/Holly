ae43360edc097aaf0a5c8806383adb7a0f3947f0
Jakub Kremláček <jkremlacek@email.cz>
2019-01-28 16:16:07 +0100

    Updated UI

--- src/main/java/cz/mzk/holly/controller/ImagepointController.java
+++ src/main/java/cz/mzk/holly/controller/ImagepointController.java
@@ -3,6 +3,7 @@ package cz.mzk.holly.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -12,8 +13,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ImagepointController {
 
     @GetMapping("/")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String greeting(Model model) {
+        model.addAttribute("uuid", "uuid:");
+        return "index";
+    }
+
+    @PostMapping("/")
+    public String download(@RequestParam(name="uuid") String uuid, Model model) {
+        model.addAttribute("uuid", uuid);
         return "index";
     }
 }
