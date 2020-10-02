66e57c0d3f78d1bdb34d12f5fe88a1b920821ffc
František Štěpanovský <stepanovsky@mzk.cz>
2020-10-02 11:23:42 +0200

    Add 2020 NDK location

--- src/main/java/cz/mzk/holly/extractor/ImageExtractor.java
+++ src/main/java/cz/mzk/holly/extractor/ImageExtractor.java
@@ -326,6 +326,9 @@ public class ImageExtractor {
                 case "2019":
                     physicalPath = path.replace("NDK", "ndk2019");
                     break;
+                case "2020":
+                    physicalPath = path.replace("NDK", "ndk2020");
+                    break;
             }
             physicalPath = Paths.get(BASE_PATH_NDK, physicalPath).toString();
         } else {
